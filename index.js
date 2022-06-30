8a64e2ceb0e1e3b7d3d6345ba16633e4100a80c0
AlamillaA7 <alexisalamilla7@gmail.com>
2022-06-29 20:51:45 -0700

    update index.js
    comments added

--- index.js
+++ index.js
@@ -3,33 +3,50 @@ const Datastore = require('nedb');
 
 const app = express();
 
-//app.listen(3000, () => console.log('listening at 3000'));
-
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log('Starting server at ${port} ');
 });
 
+
+//use express logic to set up server
+//access anything in "public" directory
+//serve static content of index.html
 app.use(express.static('public'));
+
+//understand incoming data as json
 app.use(express.json({ limit: '1mb' }));
 
+
+//create new database in local server
 const database = new Datastore('database.db');
+//create database.db file in project folder
 database.loadDatabase();
 
+
+
+ //using fetch API to POST/GET data to/from database.db
+ // '/api' is name of address to receive/return data
 app.get('/api', (request, response) => {
   database.find({}, (err, data) => {
     if (err) {
       response.end();
       return;
     }
+    //if no error, respond with data 
+    //data is usable only as json
     response.json(data);
   });
 });
 
+
+//method to post user data into db
 app.post('/api', (request, response) => {
   const data = request.body;
-  //const timestamp = Date.now();
-  //data.timestamp = timestamp;
-  database.insert(data);
+  
+  //insert data to database.db datastore
+  database.insert(data); 
+  //out data in json
   response.json(data);
 });
